866d077abd96ce51b503968996f34db2f5da4645
Porming <mingpor1607@gmail.com>
2025-05-25 19:32:06 +0700

    Init Flutter Assigment Project

---
+++ android/app/src/main/kotlin/com/example/flutter_assignment_y4_s1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_assignment_y4_s1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(title: Text("Hello Flutter")),
+        body: Center(child: Text("Welcome to Flutter")),
+      ),
+    );
+  }
+}
